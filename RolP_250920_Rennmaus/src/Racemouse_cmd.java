1f06c03c0af2970e0d068626a93cc11c6a06bc89
PhilRoli <philipprolinek@gmail.com>
2020-10-05 10:31:42 +0200

    WIP on setRaceInfo

--- RolP_250920_Rennmaus/src/Racemouse_cmd.java
+++ RolP_250920_Rennmaus/src/Racemouse_cmd.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.sound.sampled.Line;
-
 public class Racemouse_cmd {
 
     // String Linebreak used to give out empty line
@@ -18,6 +16,9 @@ public class Racemouse_cmd {
 
         // gets the number of participants
         selectParticipants();
+
+        // setting the race info
+        setRaceInfo(nParticipants);
         
     }
 
@@ -35,31 +36,63 @@ public class Racemouse_cmd {
             try{
                 nParticipants = Integer.parseInt(sParticipants);
                 if(nParticipants < 2) {
-                    System.out.println(redOutput + LineBreak + "The given number has to be bigger then 1" + resetOutput + LineBreak);
+                    System.out.println(redOutput + "The given number has to be bigger then 1" + resetOutput + LineBreak);
                     continue;
                 }
-                participantsScanner.close();
+                // ! Line 59 & 62 Crash if scanner is closed
+                // ! participantsScanner.close();
                 input = true;
             }catch (NumberFormatException ex) {
-                System.out.println(redOutput + LineBreak + "The given input has to be a whole number bigger then 1" + resetOutput + LineBreak);
+                System.out.println(redOutput + "The given input has to be a whole number bigger then 1" + resetOutput + LineBreak);
                 continue;
             }
         }
     }
 
     private static void setRaceInfo(int nParticipants) {
-        boolean input = false;
-        while(input == false) {
+        boolean nameInput = false;
+        boolean lengthInput = false;
+        boolean okayInput = false;
+        while(nameInput == false) {
             Scanner raceInfo = new Scanner(System.in);
             System.out.println("How do you want to name this Race?");
-            String raceName = raceInfo.nextLine();
-            System.out.println(LineBreak + "How long do you want your race to be? (in m)");
-            String sRaceLength = raceInfo.nextLine();
-            try{
-
-            }catch (NumberFormatException ex) {
-                
+            String raceName = raceInfo.next();
+            while(lengthInput == false) {
+                System.out.println(LineBreak + "How long do you want your race to be? (in m)");
+                String sRaceLength = raceInfo.next();
+                try{
+                    double RaceLenght = Double.parseDouble(sRaceLength);
+                    if(RaceLenght < 1) {
+                        System.out.println(redOutput + LineBreak + "The given Race Lenght has to be bigger then 0" + resetOutput + LineBreak);
+                        continue;
+                    }
+                    while(okayInput == false) {
+                        System.out.println("The Race will be named \"" + raceName + "\", and it will have a length of " + RaceLenght + "m");
+                        System.out.println("Are you okay with these infos? Enter Y for yes or N to start over again");
+                        String sOkayChoice = raceInfo.next();
+                        char okayChoice = Character.toUpperCase(sOkayChoice.charAt(0));
+                        if(okayChoice == 'Y') {
+                            // Declaration of Race with user given variables
+                            Race Race01 = new Race(raceName, nParticipants, RaceLenght);
+                            nameInput = true;
+                            lengthInput = true;
+                            okayInput = true;
+                            // * End of Function
+                            raceInfo.close();
+                            continue;
+                        }
+                        else if(okayChoice != 'N') {
+                            System.out.println("The given Input was incorrect. Please only Enter Y or N");
+                            continue;
+                        }
+                        // continue;
+                    }
+                }catch (NumberFormatException ex) {
+                    System.out.println(redOutput + LineBreak + "The given Race Lenght has to be a number bigger then 0" + resetOutput + LineBreak);
+                    continue;
+                }
             }
+                
         }
     }
 
