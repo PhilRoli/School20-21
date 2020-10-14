fe656a8d776704d290125b61a77ed2cfa9d72c0f
PhilRoli <philipprolinek@gmail.com>
2020-10-15 00:20:52 +0200

    Created new Project, more soon

---
+++ RolP_14102020_Vererbung/src/Auto.java
@@ -0,0 +1,15 @@
+public class Auto {
+
+    // marke
+    public String marke;
+
+    // Constructor
+    public Auto(String marke) {
+        this.marke = marke;
+    }
+
+    public String toString() {
+        // Attribute
+        return "Marke: " + marke; 
+    }
+}
---
+++ RolP_14102020_Vererbung/src/Cabrio.java
@@ -0,0 +1,10 @@
+public class Cabrio extends Auto {
+
+    public int vdauer; // Dach Öffnungszeit
+
+    public Cabrio(String marke, int vdauer) {
+        super(marke);
+        this.vdauer = vdauer;
+    }
+
+}
