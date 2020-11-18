4de1449c5042bbb4fc619f06052d1f19500f1e56
Philipp Rolinek <philipprolinek@gmail.com>
2020-11-18 15:16:31 +0100

    Addedn Project for today

---
+++ RolP_181120_IO_Check/src/io_checks.java
@@ -0,0 +1,11 @@
+public class io_checks {
+
+    public static void main(String[] args) {
+        // TODO
+        // Lies von einer Datei die erste Zeile ein
+        // Dort steht der Name einer Datei (name.txt)
+        // Anhand dieser zeile wird dann die angegebne datei eingelesen
+        // Gib von dieser datei den ganzen inhalt aus
+    }
+
+}
