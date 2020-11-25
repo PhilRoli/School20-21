5b163a9b8eda7b1fa4bd030b2579fed2fc9c9fc4
Philipp Rolinek <philipprolinek@gmail.com>
2020-11-25 16:16:28 +0100

    Created new Project

---
+++ RolP_181120_IO_Check/src/io_telefon.java
@@ -0,0 +1,16 @@
+public class io_telefon {
+    public static void main(String[] args) {
+        // TODO 1
+        // ${Vorname} ${Nachname} mit Tel.Nr. ${telefonnr} hat um ${betrag} telefoniert.
+        // Beispiel Datenspeicherung:
+        // nachname;vorname;adresse;telnr;betrag
+        // Bauer;Walter;5020 Salzburg;0662410000;123.45
+
+        // TODO 2
+        // Am ende gesamt Statisk anzeigen
+        // Gesamt Telefonate
+        // Gesamt Betrag
+
+        // split()
+    }
+}
