fe2601f422da8d7e981d87b88b046fc344c646b6
Philipp Rolinek <philipprolinek@gmail.com>
2020-11-29 01:53:28 +0100

    Added Table output

--- RolP_181120_IO_Check/src/io_telefon.java
+++ RolP_181120_IO_Check/src/io_telefon.java
@@ -30,6 +30,7 @@ public class io_telefon {
         phoneData = getDataArray(fileName);
         dataToOutString(phoneData);
         statistik(phoneData);
+        dataTable(phoneData);
     }
 
     public static String getDateiString() {
@@ -102,5 +103,44 @@ public class io_telefon {
 
         System.out.println("\nAnzahl Telephonate: " + telephonateAnzahl);
         System.out.println("Gesamter Betrag: " + Math.round(gesamtBetrag * 100.00) / 100.00);
+    }
+
+    public static void dataTable(ArrayList<String[]> dataArrays) {
+        // |---Nachname---|---Vorname---|---POST---|---Telefon---|---Betrag---|
+        // |--------------|-------------|----------|-------------|------------|
+        // |Rolinek-------|Philipp------|---5071---|0680 2407079-|14.90-------|
+
+        System.out.println("\n|   Nachname   |   Vorname   |   POST   |   Telefon   |   Betrag   |");
+        System.out.println("|              |             |          |             |            |");
+
+        String nachname;
+        String vorname;
+        String post;
+        String telefon;
+        String betrag;
+        char trenner = '|';
+
+        for (int i = 1; i < dataArrays.size(); i++) {
+            nachname = rightPad(dataArrays.get(i)[0], 14);
+            vorname = rightPad(dataArrays.get(i)[1], 13);
+            post = rightPad(dataArrays.get(i)[2].substring(0, 4), 10);
+            telefon = rightPad(dataArrays.get(i)[3], 13);
+            betrag = rightPad(dataArrays.get(i)[4], 12);
+
+            System.out.println(trenner + nachname + trenner + vorname + trenner + post + trenner + telefon + trenner
+                    + betrag + trenner);
+        }
+    }
+
+    public static StringBuilder rightPad(StringBuilder pStringBuilder, int pLength, char pChar) {
+        while (pStringBuilder.length() < pLength) {
+            pStringBuilder.append(pChar);
+        }
+
+        return pStringBuilder;
+    }
+
+    public static String rightPad(String pString, int pLength) {
+        return rightPad(new StringBuilder(pString), pLength, ' ').toString();
     }
 }
