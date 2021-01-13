c8b5748a18415d65b1967960aecf06bdf9512da8
Philipp Rolinek <philipprolinek@gmail.com>
2021-01-13 14:35:25 +0100

    New Project

---
+++ S20-21_CPP/cpp_pointer_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Pointer die 2.!" << endl;
+
+
+    double zahl = 0.;
+
+    // 2 Arten für den Pointer
+
+    
+
+    return 0;
+}
