eecb1634ba42be8441f9a626ade2bfacc9099e43
Philipp Rolinek <philipprolinek@gmail.com>
2021-01-17 16:10:15 +0100

    Added a 2nd setText where a char array gets put into a String

--- S20-21_CPP/cpp_pointer_2/main.cpp
+++ S20-21_CPP/cpp_pointer_2/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void setText(string text, string *pText);
+void setTextArray(char *array, string *newString);
 
 int main()
 {
@@ -10,6 +11,8 @@ int main()
 
     double zahl = 0.;
     string text;
+    string newString;
+    char cArray[] = {'N', 'e', 'u', 'e', 'r', ' ', 'S', 't', 'r', 'i', 'n', 'g', '!'};
 
     // 2 Arten für den Pointer
 
@@ -31,10 +34,23 @@ int main()
     setText("Das ist ein String!", &text);
     cout << "Text: " << text << endl;
 
+    setTextArray(cArray, &newString);
+    cout << "Text: " << newString << endl;
+
     return 0;
 }
 
 void setText(string text, string *pText)
 {
     *pText = text;
+}
+
+void setTextArray(char *array, string *newString)
+{
+    string nString(array);
+    // Aufgrund von problemen mit std::string (welches benutzt wird um das array in einen string zu formen, wird am ende von nString folgendes angehängt: ��a)
+    // Das problem bezieht sich auf folgendes: cArray gibt im Debug modus folgendes zurück "-var-create unable to create variable object" -> https://stackoverflow.com/questions/41854840/var-create-unable-to-create-variable-object
+    // Um dieses problem auf dem einfachsten weg zu umgehen, schneiden wir die letzten 3 chars des strings ab
+    // Die andere möglichkeit dieses Problem zu lösen, wäre indem man zuerst einen leeren string erstellet, und diesen dann durch eine schleife mit den einzelenen chars befüllt.
+    *newString = nString.substr(0, nString.size() - 3);
 }
