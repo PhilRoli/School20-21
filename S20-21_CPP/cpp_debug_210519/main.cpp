9a100d21c09895fddcb0f996134865358ba6c983
Philipp Rolinek <philipprolinek@gmail.com>
2021-05-19 16:07:25 +0200

    New Project

---
+++ S20-21_CPP/cpp_debug_210519/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Debug" << endl;
+
+    int pi = 3.1415;
+
+    double wert = sin(pi);
+
+    cout << "Wert: " << wert << endl;
+
+    return 0;
+}
